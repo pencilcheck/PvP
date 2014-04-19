673e8c76b5807ff6d1f7dcfca7aad1f987e9eec5
Penn Su <penn@gameyourgame.com>
2014-04-18 21:34:32 -0700

    Part of game

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -16,8 +16,9 @@ angular.module('PvP')
       var Gameinfo = new Game()
       var invited = game.state >= GameStates.invitesSent && Array.isArray(game.invitations) && game.invitations.indexOf(UserSession.currentUser().uid) != -1
       var gamePublic = game.state >= GameStates.invitesSent && _.keys(game.participants).length < Gameinfo._limit && !_.has(game, 'invitations')
+      var partOfGame = game.state >= GameStates.movesPicked && game.participants[UserSession.currentUser().uid]
       var yourthehost = game.host && game.host.uid == UserSession.currentUser().uid
-      return gamePublic || invited || yourthehost
+      return gamePublic || invited || partOfGame || yourthehost
     }
 
     games.$getIndex().forEach(function (index) {
